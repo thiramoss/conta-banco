0c5f26f60f476afe4dc30880bc8d6808bd1ca96a
thiramoss <trs.tiba@gmail.com>
2025-04-11 01:32:47 -0300

    commitando

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,22 +1,19 @@
-import java.util.Scanner; // Importação necessária para usar o Scanner
+import java.util.Scanner;
 
 public class ContaTerminal {
     public static void main(String[] args) {
-        // Criando o objeto Scanner para ler entrada do usuário
         Scanner scanner = new Scanner(System.in);
 
-        // Declaração das variáveis
         int numConta;
         String numAgencia;
         String nomeCliente;
         double saldo;
 
-        // Solicitando e lendo os dados do usuário
         System.out.println("Qual o seu nome: ");
-        nomeCliente = scanner.nextLine(); // Lê o nome completo
+        nomeCliente = scanner.nextLine();
 
         System.out.println("Digite o número da agência: ");
-        numAgencia = scanner.nextLine(); // Lê a agência (pode conter caracteres como '-')
+        numAgencia = scanner.nextLine();
 
         System.out.println("Digite o Número da conta: ");
         numConta = scanner.nextInt();
